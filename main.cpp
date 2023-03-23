3ac406f79db47facce34b9dadca4f778488e9bc3
Colin Nordquist <colin.james.nordquist@gmail.com>
2023-03-22 23:45:12 -0700

    basic skeleton of the CLI is taking shape in main.

--- main.cpp
+++ main.cpp
@@ -22,94 +22,47 @@ int main() {
 	//perft.run();
 
 	// Kiwipete
-	int depth = 4;
-	Perft perft = Perft(Position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), depth);
-	perft.run();
+	// int depth = 4;
+	// Perft perft = Perft(Position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), depth);
+	// perft.run();
 	//perft = Perft(Position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1").mirror_position(), depth);
 	//perft.run();
 
-	//using namespace std;
-	//string token;
-	//bool quit = false;
+	Position current_position = Position();
+	std::string token;
+	bool running = true;
 
+	// welcome message
+	std::cout << "V8 by Colin Nordquist" << std::endl << std::endl;
 
-	//// welcome message
-	//cout << "V8 by Colin Nordquist" << endl;
+	while (running) {
+		std::cout << "v8 > " << std::endl;
 
-	//while (!quit) {
-	//	int depth;
-	//	string filepath, fen;
-	//	bool view, sort, no_summary;
-	//	array<Types, 6> piece_filter;
-	//	array<char, 2> square_filter;
-	//	array<MoveTypes, 9> move_filter;
-	//	array<MoveResults, 4> special;
+		std::cin >> token;
+		if (token == "setup") {
+			std::string fen;
+			std::cin >> fen;
+			current_position = Position(fen);
+		}
+		else if (token == "display") {
 
-	//	cout << "v8 > " << endl;
-	//	
-	//	cin >> token;
-	//	if (token == "perft") {
-	//		// default values
-	//		fen = STARTING_POSITION;
-	//		depth = 1;
-	//		view = false;
+		}
+		else if (token == "move") {
 
-	//		cin >> filepath;
-	//		cin >> token;
-	//		while (!cin.eof()) {
-	//			if ((token == "--position" || token == "-p") && fen == STARTING_POSITION) {
-	//				cin >> fen;
-	//			}
-	//			if ((token == "--depth" || token == "-d") && depth == 1) {
-	//				cin >> depth;
-	//			}
-	//			if ((token == "--view" || token == "-v") && view == false) {
-	//				view = true;
-	//			}
-	//			cin >> token;
-	//		}
-	//	}
-	//	else if (token == "view") {
-	//		// default values
-	//		std::fill_n(piece_filter, piece_filter.size(), Types::NONE);
-	//		square_filter = { 0, 0 };
-	//		std::fill_n(move_filter, move_filter.size(), MoveTypes::NONE);
-	//		std::fill_n(special, special.size(), MoveResults::NONE);
-	//		sort = false;
-	//		no_summary = false;
+		}
+		else if (token == "undo") {
 
-	//		cin >> filepath;
-	//		cin >> token;
-	//		while (!cin.eof()) {
-	//			if ((token == "--filter-pieces" || token == "-fp") && piece_filter[0] == Types::NONE) {
-	//				cin >> fen;
-	//			}
-	//			if ((token == "--filter-squares" || token == "-fs") && square_filter[0] == 0) {
-	//				cin >> depth;
-	//			}
-	//			if ((token == "--filter-moves" || token == "-fm") && move_filter[0] == MoveTypes::NONE) {
-	//				view = true;
-	//			}
-	//			if ((token == "--special" || token == "-sp") && fen == STARTING_POSITION) {
-	//				cin >> fen;
-	//			}
-	//			if ((token == "--sort" || token == "-s") && fen == STARTING_POSITION) {
-	//				cin >> fen;
-	//			}
-	//			if ((token == "--no-summary" || token == "-ns") && fen == STARTING_POSITION) {
-	//				cin >> fen;
-	//			}
-	//			cin >> token;
-	//		}
-	//	}
+		}
+		else if (token == "perft") {
 
-	//	
-	//	cout << "Stream state - " << endl;
-	//	cout << "Good: " << cin.good() << endl;
-	//	cout << "Fail: " << cin.fail() << endl;
-	//	cout << "Bad: " << cin.bad() << endl;
+		}
+		else if (token == "quit") {
 
-	//	cout << perft_cmd;
+		}
+		else {
+
+		}
+	}	
 }
 //	//Position P = Position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); //Starting position
 //	Position P = Position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"); //Kiwipete position
